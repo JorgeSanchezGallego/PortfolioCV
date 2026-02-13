573573085f54fc58e3cf8e41ab3ccdd743cd48ca
Jorge Sánchez Gallego <jorgesanchezluky@gmail.com>
2026-02-13 18:37:15 +0100

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+    <section className='hero-section'> 
+        <div className='hero-container'>
+            <h1 className='title-hero'>JORGE SÁNCHEZ</h1>
+            <h3 className='info-hero'>Desarrollador FullStack</h3>
+        </div>
+    </section>
+    )
+}
+
+export default Hero
